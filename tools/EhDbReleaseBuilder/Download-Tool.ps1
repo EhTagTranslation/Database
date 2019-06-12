7815be58a7d6ea9d123bc82cdc6bcdbca1ed7208
OpportunityLiu <opportunity@live.in>
2019-06-12 18:44:41 +0800

    update script

--- tools/EhDbReleaseBuilder/Download-Tool.ps1
+++ tools/EhDbReleaseBuilder/Download-Tool.ps1
@@ -2,5 +2,4 @@ $Release = Invoke-WebRequest 'https://api.github.com/repos/EhTagTranslation/EhTa
 
 $zipfile = "$PSScriptRoot/EhDbReleaseBuilder.zip"
 Invoke-WebRequest $Release.assets[0].browser_download_url -OutFile $zipfile
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-[System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $PSScriptRoot)
+Expand-Archive $zipfile $PSScriptRoot
