46165343107cdccb55b3cb99cdf3ba59289134fd
OpportunityLiu <opportunity@live.in>
2019-08-07 14:38:02 +0800

    improve script

--- tools/EhDbReleaseBuilder/Download-Tool.ps1
+++ tools/EhDbReleaseBuilder/Download-Tool.ps1
@@ -1,5 +1,8 @@
-$Release = Invoke-WebRequest 'https://api.github.com/repos/EhTagTranslation/EhTagConnector/releases/latest' -Headers @{ Authorization = "token $($env:GitHub:Token)" } | ConvertFrom-Json
+#! /bin/env pwsh
+#Requires -Version 5
+
+$Release = Invoke-WebRequest 'https://api.github.com/repos/EhTagTranslation/EhTagConnector/releases/latest' -Headers @{ Authorization = "token ${env:GitHub:Token}" } | ConvertFrom-Json
 
 $zipfile = "$PSScriptRoot/EhDbReleaseBuilder.zip"
 Invoke-WebRequest $Release.assets[0].browser_download_url -OutFile $zipfile
-Expand-Archive $zipfile $PSScriptRoot
+Expand-Archive $zipfile $PSScriptRoot -Force
